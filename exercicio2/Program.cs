295c22601185eeddb8a1d832dacee130960c9f4f
Hudson <Hudson@BRSTORE>
2022-02-21 15:45:58 -0300

    Adicionar arquivos de projeto.

---
+++ exercicio2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace exercicio2 {
+    class Program {
+        static void Main(string[] args) {
+
+
+            Console.WriteLine("PROGRAMA QUE DEFINE QUANTAS PESSOAS TEM MAIS DE 18 ANOS");
+            Console.WriteLine("--------------------------------------------------------");
+            Console.WriteLine();
+
+            int qtde = 0;
+            int idade = 0;
+            
+            int i = 0;
+            while (i < 10) {
+                i = i + 1;
+                Console.Write("Informe a idade da pessoa: ");
+                idade = Convert.ToInt32(Console.ReadLine());
+                if (idade >= 18) {
+                    qtde = qtde + 1;
+                }
+            }
+            Console.WriteLine("Existem " + qtde + " pessoas com mais de 18 anos");
+            Console.WriteLine();
+            Console.WriteLine("****FIM DO PROGRAMA****");
+            Console.ReadKey();
+        }
+
+       
+
+    }
+
+}
